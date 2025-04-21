415300e1ec585adc507471736b3ff06894aa376b
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2025-04-21 16:40:13 -0300

    Feat: altera cor dos icone e texto das tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
+import { Text } from "react-native";
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -12,12 +13,25 @@ function TabBarIcon(props: {
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{}}>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: true,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Novas Tarefas",
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          tabBarLabel: ({ focused }) => (
+            <Text
+              style={{ color: focused ? "#fd5ba9" : "#fd5ba94a", fontSize: 12 }}
+            >
+              Novas Tarefas
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon name="home" color={focused ? "#fd5ba9" : "#fd5ba94a"} />
+          ),
           headerShown: false,
         }}
       />
@@ -26,18 +40,39 @@ export default function TabLayout() {
         name="inProgress"
         options={{
           title: "Em Andamento",
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="calendar-o" color={color} />
+          tabBarLabel: ({ focused }) => (
+            <Text
+              style={{ color: focused ? "#855bfd" : "#865bfd4a", fontSize: 12 }}
+            >
+              Em Andamento
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              name="calendar-o"
+              color={focused ? "#855bfd" : "#865bfd4a"}
+            />
           ),
           headerShown: false,
         }}
       />
+
       <Tabs.Screen
         name="finished"
         options={{
           title: "Finalizados",
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name="check-circle" color={color} />
+          tabBarLabel: ({ focused }) => (
+            <Text
+              style={{ color: focused ? "#05521d" : "#05521d4a", fontSize: 12 }}
+            >
+              Finalizados
+            </Text>
+          ),
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              name="check-circle"
+              color={focused ? "#05521d" : "#05521d4a"}
+            />
           ),
           headerShown: false,
         }}
